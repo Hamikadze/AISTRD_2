d3faa3d2028052d9cc77422534315682311f62a4
Hamikadze <best.homa@gmail.com>
2019-04-01 01:24:09 +0300

    full working proof of concept

--- Project_3/Project_3.cpp
+++ Project_3/Project_3.cpp
@@ -1,4 +1,4 @@
-// Project_3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Project_3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -6,12 +6,12 @@
 
 static bool count_compare(const void* elem1, const void* elem2)
 {
-	auto a = (Node<char, string>*)(elem1);
-	auto b = (Node<char, string>*)elem2;
+	auto a = (Node<wchar_t, wstring>*)(elem1);
+	auto b = (Node<wchar_t, wstring>*)elem2;
 	return (a->get_count() /*<=*/ > b->get_count());
 }
 
-void EncShannon(Dictionary<char, string>& letters, LinkedList<Node<char, std::string>*>& nodes, int lenght, int left_index, int right_index)
+void ShannonCodes(Dictionary<wchar_t, wstring>& letters, LinkedList<Node<wchar_t, std::wstring>*>& nodes, int lenght, int left_index, int right_index)
 {
 	if (left_index == right_index)
 	{
@@ -21,58 +21,90 @@ void EncShannon(Dictionary<char, string>& letters, LinkedList<Node<char, std::st
 	{
 		//  If interval consist of 2 elements then it's simple
 		//
-		letters.Find(nodes.at(left_index)->get_key())->append("0");
-		letters.Find(nodes.at(right_index)->get_key())->append("1");
+		letters.Find(nodes.at(left_index)->get_key())->append(L"0");
+		letters.Find(nodes.at(right_index)->get_key())->append(L"1");
 	}
 	else
 	{
 		//  Calculating sum of probabilities at specified interval
 		//
-		float pfull = 0;
+		float p_full = 0;
 		for (int i = left_index; i <= right_index; ++i)
 		{
-			pfull += nodes.at(i)->get_count() / (float)lenght;
+			p_full += nodes.at(i)->get_count() / static_cast<float>(lenght);
 		}
 
 		//  Searching center
 		//
 		float p = 0;
-		int isp = -1; // index of split pos
-		float phalf = pfull * 0.5f;
+		int split_index = -1; // index of split pos
+		const float p_half = p_full * 0.5f;
 		for (int i = left_index; i <= right_index; ++i)
 		{
-			p += nodes.at(i)->get_count() / (float)lenght;
-			if (p <= phalf) {
-				letters.Find(nodes.at(i)->get_key())->append("0");
+			p += nodes.at(i)->get_count() / static_cast<float>(lenght);
+			if (p <= p_half) {
+				letters.Find(nodes.at(i)->get_key())->append(L"0");
 			}
 			else
 			{
-				letters.Find(nodes.at(i)->get_key())->append("1");
-				if (isp < 0) isp = i;
+				letters.Find(nodes.at(i)->get_key())->append(L"1");
+				if (split_index < 0) split_index = i;
 			}
 		}
 
-		if (isp < 0) isp = left_index + 1;
+		if (split_index < 0) split_index = left_index + 1;
 
 		//  Next step (recursive)
 		//
-		EncShannon(letters, nodes, lenght, left_index, isp - 1);
-		EncShannon(letters, nodes, lenght, isp, right_index);
+		ShannonCodes(letters, nodes, lenght, left_index, split_index - 1);
+		ShannonCodes(letters, nodes, lenght, split_index, right_index);
 	}
+}
+
+std::wstring Encode(std::wstring & input, Dictionary<wchar_t, wstring> & table)
+{
+	std::wstring output;
+	for (wchar_t letter : input)
+	{
+		output.append(*table.Find(letter));
+	}
+	return output;
+}
+
+std::wstring Decode(std::wstring input, Dictionary<wchar_t, wstring> & table)
+{
+	std::wstring output;
+	while (input.length() > 0)
+	{
+		auto iterator = table.Nodes().create_list_iterator();
+		while (iterator->has_next())
+		{
+			auto code = iterator->next();
+			unsigned int code_length = code->get_value().length();
+			if (input.compare(0, code_length, code->get_value()) == 0)
+			{
+				output += code->get_key();
+				input = input.substr(code_length);
+			};
+		}
+	};
+	return output;
 }
 
 int main()
 {
-	std::string test_string = "aaaaaaaassbbbdddd";
-	Dictionary<char, string> letters;
-	for (char letter : test_string)
+	setlocale(LC_ALL, "Russian");
+	std::wstring test_string = L"гигабайт";
+	Dictionary<wchar_t, wstring> letters;
+	for (wchar_t letter : test_string)
 	{
-		letters.Insert(letter, "");
+		letters.Insert(letter, L"");
 	}
 	auto nodes = letters.Nodes();
 	nodes.sort(count_compare);
-	EncShannon(letters, nodes, test_string.length(), 0, letters.get_size() - 1);
-
+	ShannonCodes(letters, nodes, test_string.length(), 0, letters.get_size() - 1);
+	auto encode_str = Encode(test_string, letters);
+	auto decode_str = Decode(encode_str, letters);
 	std::cout << "Hello World!\n";
 	return 0;
 }
