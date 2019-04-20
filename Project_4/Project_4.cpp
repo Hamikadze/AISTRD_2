6785e4afeee1c959da783ffb1060b492542d8bcf
Hamikadze <best.homa@gmail.com>
2019-04-20 21:19:56 +0300

    refactoring

--- Project_4/Project_4.cpp
+++ Project_4/Project_4.cpp
@@ -6,46 +6,63 @@
 #include <iostream>
 #include <vector>
 
-void addEdge(Dictionary<int, LinkedList<int>>& adj, int u, int v)
+class graph_node
 {
-	LinkedList<int>* f_result;
-	if (adj.Find(u, f_result))
+public:
+	int dest;
+	int weight;
+	graph_node()
 	{
-		(*f_result).push_back(v);
+		int dest = weight = 0;
+	}
+	graph_node(int dest, int weight)
+	{
+		this->dest = dest;
+		this->weight = weight;
+	}
+};
+
+void addEdge(Dictionary<int, LinkedList<graph_node>>& adj, int source, int dest, int weight)
+{
+	graph_node node(dest, weight);
+	LinkedList<graph_node>* f_result;
+	if (adj.Find(source, f_result))
+	{
+		(*f_result).push_back(node);
 	}
 	else
 	{
-		LinkedList<int> value;
-		value.push_back(v);
-		adj.Insert(u, value);
+		LinkedList<graph_node> value;
+		value.push_back(node);
+		adj.Insert(source, value);
 	}
 
-	if (adj.Find(v, f_result))
-	{
-		(*f_result).push_back(u);
-	}
-	else
-	{
-		LinkedList<int> value;
-		value.push_back(u);
-		adj.Insert(v, value);
-	}
+	//if (adj.Find(dest, f_result))
+	//{
+	//	(*f_result).push_back(source);
+	//}
+	//else
+	//{
+	//	LinkedList<int> value;
+	//	value.push_back(source);
+	//	adj.Insert(dest, value);
+	//}
 }
 
-void printGraph(Dictionary<int, LinkedList<int>> adj, int V)
+void printGraph(Dictionary<int, LinkedList<graph_node>> adj, int V)
 {
 	for (int v = 0; v < V; ++v)
 	{
 		cout << "\n Adjacency list of vertex "
 			<< v << "\n head ";
 
-		LinkedList<int>* f_result;
+		LinkedList<graph_node>* f_result;
 		if (adj.Find(v, f_result))
 		{
 			auto iterator = f_result->create_list_iterator();
 			while (iterator->has_next())
 			{
-				cout << "-> " << iterator->next();
+				cout << "-> " << iterator->next().dest;
 			}
 		}
 		printf("\n");
@@ -55,14 +72,14 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int V = 5;
-	Dictionary<int, LinkedList<int>> adj;
-	addEdge(adj, 0, 1);
-	addEdge(adj, 0, 4);
-	addEdge(adj, 1, 2);
-	addEdge(adj, 1, 3);
-	addEdge(adj, 1, 4);
-	addEdge(adj, 2, 3);
-	addEdge(adj, 3, 4);
+	Dictionary<int, LinkedList<graph_node>> adj;
+	addEdge(adj, 0, 1, 1);
+	addEdge(adj, 0, 4, 1);
+	addEdge(adj, 1, 2, 1);
+	addEdge(adj, 1, 3, 1);
+	addEdge(adj, 1, 4, 1);
+	addEdge(adj, 2, 3, 1);
+	addEdge(adj, 3, 4, 1);
 	printGraph(adj, V);
 	return 0;
 }
